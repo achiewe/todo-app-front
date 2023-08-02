f96b365902d179aacca7c8016d045030cb9debd7
achiewe <mamukashviliachi706@gmail.com>
2023-08-02 19:38:12 +0400

    add input styles

--- src/components/InputSaveBar.tsx
+++ src/components/InputSaveBar.tsx
@@ -22,16 +22,46 @@ const InputSaveBar = ({ takeData }: propsBar): JSX.Element => {
 
   return (
     <Form onSubmit={addTask}>
-      <input id="searchPlace" />
+      <button></button>
+      <input id="searchPlace" placeholder="Create a new todo…" />
     </Form>
   );
 };
 
 const Form = styled.form`
   width: 327px;
+  background-color: #ffffff;
+  padding: 14px 0 14px 20px;
+  display: flex;
+  border: none;
+  border-radius: 5px;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+
+  button {
+    width: 20px;
+    height: 20px;
+    background: #ffffff;
+    border-radius: 50%;
+    border: 1px solid #e3e4f1;
+  }
 
   input {
     width: 100%;
+    border: none;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 12px;
+    letter-spacing: -0.1666666716337204px;
+    text-align: left;
+    color: black;
+    background-color: #ffffff;
+    outline: none;
+  }
+
+  input::placeholder {
+    color: #9495a5;
   }
 `;
 
